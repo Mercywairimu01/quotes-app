621861d16ad1c3ba1c63f98b2a514b5a06776e15
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-04-09 18:09:35 +0300

    add a delete button that deletes the quote from the array

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -8,6 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteDetailsComponent implements OnInit {
    @Input() quotes!:Quote;
+   @Output ()isComplete = new EventEmitter<boolean>();
+   
+   quotesComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
   ngOnInit(): void {
