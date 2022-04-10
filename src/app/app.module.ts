40f80828a7c6860b5546e8f1bf16925d392310a5
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-04-10 09:55:46 +0300

    create a time pipe to calculate time since the quote was created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { TimeCountPipe } from './time-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    TimeCountPipe
   ],
   imports: [
     BrowserModule,
