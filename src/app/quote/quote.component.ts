fe741156a4cc2bfdb2d2a3609d8950014c74d0e5
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-04-10 10:27:12 +0300

    add logic to calculate time lapsed since quote was created

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,16 +8,17 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] =[
-   new Quote(1,'quote1','Submitted by:',new Date(2022,4,6)),
-   new Quote(2,'quote2','Submitted by:',new Date(2022,5,6)),
-   new Quote(3,'quote3','Submitted by:',new Date(2022,6,6)),
-   new Quote(4,'quote4','Submitted by:',new Date(2022,2,6))
+   new Quote(1,'quote1','Submitted by:',new Date(2022,3,6)),
+   new Quote(2,'quote2','Submitted by:',new Date(2022,3,11)),
+   new Quote(3,'quote3','Submitted by:',new Date(2022,2,6)),
+   new Quote(4,'quote4','Submitted by:',new Date(2022,3,6))
     
   ];
   toggleDetails(index:number){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
   completeQuote(isComplete: any, index: number){
+    
     if (isComplete) {
       this.quotes.splice(index,1);
     }
